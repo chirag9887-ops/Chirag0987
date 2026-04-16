3d46e674332403e34ba62d6fab0d336322f37a85
chirag9887-ops <chirag08876@gmail.com>
2026-04-16 10:18:38 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-
-const server = http.createServer((req, res) => {
-  res.end("App chal raha hai 🚀");
-});
-
-server.listen(3000, () => {
-  console.log("Server started");
-});
